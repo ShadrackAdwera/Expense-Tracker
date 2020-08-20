956001b0e58d11a98dba448be9312f4682f260da
FACTS  LIMITED <admin@factsafrica.com>
2020-08-20 15:14:17 +0300

    add a table to view categories

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react';
+import MUIDataTable from 'mui-datatables';
+import { useHttp } from '../../shared/http-hook';
+import './welcome.css';
 
 const Welcome = () => {
-    return <div>
-        <h1>Welcome</h1>
-    </div>
-}
+  const columns = ['name', 'description', 'price', 'date', 'category'];
+  const options = {
+    filterType: 'checkbox',
+  };
 
-export default Welcome
+  const [expenses, setExpenses] = useState([]);
+  const { sendRequest } = useHttp();
+  const fetchExpenses = useCallback(async () => {
+    try {
+      const resData = await sendRequest(
+        'http://localhost:5000/api/expenses/all'
+      );
+      setExpenses(resData.expenses);
+    } catch (error) {}
+  }, [sendRequest]);
+
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
+
+  return (
+    <div className="main-container">
+      <div className="child-container">
+        <MUIDataTable
+          title={'Expenses'}
+          data={expenses}
+          columns={columns}
+          options={options}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
