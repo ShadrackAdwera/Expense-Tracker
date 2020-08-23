60daa3d266b6f4188dcc448c934ab0dbc13742ef
FACTS  LIMITED <admin@factsafrica.com>
2020-08-23 13:42:48 +0300

    code clean up on the expenses controller

--- baccend/controllers/expenses-controllers.js
+++ baccend/controllers/expenses-controllers.js
@@ -1,46 +1,45 @@
-const { v4: uuid } = require('uuid');
 const { validationResult } = require('express-validator');
 const moment = require('moment');
 const Expense = require('../models/Expense');
 
 const HttpError = require('../models/http-error');
 
-let DUMMY_EXPENSES = [
-  {
-    id: uuid(),
-    name: 'Morning Expense',
-    description:
-      'Bought Nyama, Ngwashe, Ndizi, Unga, Mboga, Nyanya, Kitunguu, Pili Pili',
-    price: 1000,
-    image:
-      'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
-    date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
-    category: uuid(),
-    user: 1,
-  },
-  {
-    id: uuid(),
-    name: 'Afternoon Expense',
-    description: 'Ordered Pizza Off of Jumia',
-    price: 1200,
-    image:
-      'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
-    date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
-    category: uuid(),
-    user: 2,
-  },
-  {
-    id: uuid(),
-    name: 'Night Expense',
-    description: 'Enjoyed Nyama Choma and Drinks with friends',
-    price: 1500,
-    image:
-      'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
-    date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
-    category: uuid(),
-    user: 1,
-  },
-];
+// let DUMMY_EXPENSES = [
+//   {
+//     id: uuid(),
+//     name: 'Morning Expense',
+//     description:
+//       'Bought Nyama, Ngwashe, Ndizi, Unga, Mboga, Nyanya, Kitunguu, Pili Pili',
+//     price: 1000,
+//     image:
+//       'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
+//     date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
+//     category: uuid(),
+//     user: 1,
+//   },
+//   {
+//     id: uuid(),
+//     name: 'Afternoon Expense',
+//     description: 'Ordered Pizza Off of Jumia',
+//     price: 1200,
+//     image:
+//       'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
+//     date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
+//     category: uuid(),
+//     user: 2,
+//   },
+//   {
+//     id: uuid(),
+//     name: 'Night Expense',
+//     description: 'Enjoyed Nyama Choma and Drinks with friends',
+//     price: 1500,
+//     image:
+//       'https://i.pinimg.com/originals/75/52/ca/7552ca31d6707a32d67ae0fea789ac44.jpg',
+//     date: moment(new Date()).format('MMMM Do YYYY, hh:mm'),
+//     category: uuid(),
+//     user: 1,
+//   },
+// ];
 
 //CREATE
 const addExpense = async (req, res, next) => {
@@ -66,69 +65,73 @@ const addExpense = async (req, res, next) => {
   } catch (error) {
     return next(new HttpError('Could not add expense', 500));
   }
-  res
-    .status(201)
-    .json({
-      message: 'Expense Added',
-      expense: createdExpense.toObject({ getters: true }),
-    });
+  res.status(201).json({
+    message: 'Expense Added',
+    expense: createdExpense.toObject({ getters: true }),
+  });
 };
 
 //READ
 const getAllExpenses = async (req, res, next) => {
-  let allExpenses
+  let allExpenses;
   try {
     allExpenses = await Expense.find().exec();
   } catch (error) {
-    return next(new HttpError('Could not fetch expenses',500))
+    return next(new HttpError('Could not fetch expenses', 500));
   }
   res
     .status(200)
-    .json({ totalExpenses: allExpenses.length, expenses: allExpenses.map(expe=>expe.toObject({getters:true})) });
+    .json({
+      totalExpenses: allExpenses.length,
+      expenses: allExpenses.map((expe) => expe.toObject({ getters: true })),
+    });
 };
 
 const getExpensesByUser = async (req, res, next) => {
   const userId = req.params.id;
-  let userExpenses
+  let userExpenses;
 
   try {
-    userExpenses = await Expense.find({user: userId}).exec()
+    userExpenses = await Expense.find({ user: userId }).exec();
   } catch (error) {
-    return next(new HttpError('Could not fetch user',500))
+    return next(new HttpError('Could not fetch user', 500));
   }
-  if(!userExpenses ||userExpenses.length<1) {
-    return next(new HttpError('Could not find expenses for the provided user',404))
+  if (!userExpenses || userExpenses.length < 1) {
+    return next(
+      new HttpError('Could not find expenses for the provided user', 404)
+    );
   }
   res
     .status(200)
-    .json({ totalExpenses: userExpenses.length, expenses: userExpenses.map(exp=>exp.toObject({getters:true})) });
-
-
+    .json({
+      totalExpenses: userExpenses.length,
+      expenses: userExpenses.map((exp) => exp.toObject({ getters: true })),
+    });
 };
 
 const getExpenseById = async (req, res, next) => {
   const expenseId = req.params.id;
-  let foundExpense
+  let foundExpense;
   try {
-    foundExpense = await Expense.findById(expenseId).exec()
+    foundExpense = await Expense.findById(expenseId).exec();
   } catch (error) {
-    return next(new HttpError('Could not fetch expense', 500))
+    return next(new HttpError('Could not fetch expense', 500));
   }
-  if(!foundExpense) {
-    return next(new HttpError('Expense does not exist', 404))
+  if (!foundExpense) {
+    return next(new HttpError('Expense does not exist', 404));
   }
-  res.status(200).json({ expense: foundExpense.toObject({getters:true}) });
+  res.status(200).json({ expense: foundExpense.toObject({ getters: true }) });
 };
 
 //UPDATE
 const updateExpense = async (req, res, next) => {
   const expenseId = req.params.id;
   const { name, description, price, date, category } = req.body;
-  
-  let foundExpense
+
+  let foundExpense;
 
   try {
-    foundExpense = await Expense.findById(expenseId)
+    foundExpense = await Expense.findById(expenseId);
   } catch (error) {
     return next(new HttpError('Could not fetch expense', 500));
   }
@@ -143,20 +146,25 @@ const updateExpense = async (req, res, next) => {
   foundExpense.category = category;
 
   try {
-    await foundExpense.save()
+    await foundExpense.save();
   } catch (error) {
     return next(new HttpError('Could not save expense', 500));
   }
 
-  res.status(200).json({ message: 'Updated!', expense: foundExpense.toObject({getters:true}) });
+  res
+    .status(200)
+    .json({
+      message: 'Updated!',
+      expense: foundExpense.toObject({ getters: true }),
+    });
 };
 
 const deleteExpense = async (req, res, next) => {
   const expenseId = req.params.id;
-  let foundExpense
+  let foundExpense;
 
   try {
-    foundExpense = await Expense.findById(expenseId)
+    foundExpense = await Expense.findById(expenseId);
   } catch (error) {
     return next(new HttpError('Could not fetch expense', 500));
   }
@@ -164,7 +172,7 @@ const deleteExpense = async (req, res, next) => {
     return next(new HttpError('Expense does not exist', 404));
   }
   try {
-    await Expense.remove(foundExpense)
+    await Expense.remove(foundExpense);
   } catch (error) {
     return next(new HttpError('Could not delete expense', 500));
   }
