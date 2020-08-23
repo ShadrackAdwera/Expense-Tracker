77335dc65eddfb009fef9e69305df5db6a69e824
FACTS  LIMITED <admin@factsafrica.com>
2020-08-23 15:42:34 +0300

    Create a category schema

--- baccend/models/Category.js
+++ baccend/models/Category.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    expenses: [{type: mongoose.Types.ObjectId, required: true, ref:'Expense'}]
-})
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  expenses: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Expense' }],
+});
 
-module.exports = mongoose.model('Category',categorySchema)
+module.exports = mongoose.model('Category', categorySchema);
