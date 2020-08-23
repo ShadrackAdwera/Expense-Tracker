3f1d88446dcf32d566217316e78c2c41a6070e41
FACTS  LIMITED <admin@factsafrica.com>
2020-08-23 13:00:48 +0300

    fix unified topology, new uri parser warning on mongo db

--- baccend/app.js
+++ baccend/app.js
@@ -1,59 +1,64 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const fs = require('fs')
-const mongoose = require('mongoose')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const mongoose = require('mongoose');
 
-const HttpError = require('./models/http-error')
-const userRoutes = require('./routes/user-routes')
-const categoriesRoutes = require('./routes/categories-routes')
-const expensesRoutes = require('./routes/expenses-routes')
+const HttpError = require('./models/http-error');
+const userRoutes = require('./routes/user-routes');
+const categoriesRoutes = require('./routes/categories-routes');
+const expensesRoutes = require('./routes/expenses-routes');
 
-const app = express()
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'PUT, PATCH, POST, DELETE, GET'
-    );
-    next();
-  });
-
-app.use(bodyParser.json())
-
-app.use('/uploads/images', express.static(path.join('uploads','images')))
-
-app.use('/api/users', userRoutes)
-app.use('/api/categories', categoriesRoutes)
-app.use('/api/expenses',expensesRoutes)
-
+const app = express();
 
 app.use((req, res, next) => {
-    throw new HttpError('Could not find the method / route. Try Again', 500);
-  });
-  
-  app.use((error, req, res, next) => {
-    if(req.file){
-      fs.unlink(req.file.path, (err)=>{
-        console.log(err)
-      })
-    }
-    if (res.headerSent) {
-      return next(error);
-    }
-    res
-      .status(error.code || 500)
-      .json({ error: error.message || 'An error occured, try again' });
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'PUT, PATCH, POST, DELETE, GET'
+  );
+  next();
+});
 
-  mongoose.connect(`mongodb+srv://adwera:${process.env.DB_PASSWORD}@mongo-rest-api.0iss4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(()=>{
-    console.log('Connected to database')
-    app.listen(5000)
-  }).catch(()=>{
-    console.log('Error connecting...')
+app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use('/api/users', userRoutes);
+app.use('/api/categories', categoriesRoutes);
+app.use('/api/expenses', expensesRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError('Could not find the method / route. Try Again', 500);
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ error: error.message || 'An error occured, try again' });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://adwera:${process.env.DB_PASSWORD}@mongo-rest-api.0iss4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(5000);
   })
+  .catch(() => {
+    console.log('Error connecting...');
+  });
